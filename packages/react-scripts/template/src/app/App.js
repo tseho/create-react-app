427105cc6f6f9881dc651d570c386f32352528df
Tseho <quentin.favrie@gmail.com>
2017-03-12 00:54:40 +0100

    dev: renames App.css to App.scss

--- packages/react-scripts/template/src/app/App.js
+++ packages/react-scripts/template/src/app/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { IndexLink, Link } from 'react-router'
 import logo from './logo.svg'
-import './App.css'
+import './App.scss'
 
 class App extends Component {
     render () {
